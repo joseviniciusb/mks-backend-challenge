6eb966e87f40e098bdf5a9b001e580fc6a4e3106
joseviniciusb <josevbluz@outlook.com>
2024-05-02 00:38:26 -0300

    ft: create post requisition | add Http code status

--- src/film-catalog/film-catalog.controller.ts
+++ src/film-catalog/film-catalog.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
 
 @Controller()
 export class FilmCatalogController {
   @Get('film-catalog')
-  findAll() {
-    return 'Listagem de filmes';
+  findAll(@Res() response ) {
+    return response.status(200).send('Catálogo de filmes');
   }
 
   @Get('film-catalog/:id')
   findOne(@Param() params) {
     return `Filme: ${params.id}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() body) {
+    return body;
   }
 }
