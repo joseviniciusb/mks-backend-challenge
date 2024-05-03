11fa278659a6e0f238a9a29704b6c7efffadef9d
joseviniciusb <josevbluz@outlook.com>
2024-05-03 13:42:41 -0300

    ft: Inject services into the controller

--- src/film-catalog/film-catalog.controller.ts
+++ src/film-catalog/film-catalog.controller.ts
@@ -3,49 +3,38 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
   Patch,
   Post,
-  Res,
 } from '@nestjs/common';
 import { FilmCatalogService } from './film-catalog.service';
 
 @Controller()
 export class FilmCatalogController {
-
-  constructor(private readonly listCatalogService: FilmCatalogService) {
-
-  }
+  constructor(private readonly listCatalogService: FilmCatalogService) {}
 
   @Get('film-catalog')
-  findAll(@Res() response) {
-    return response.status(200).send('Catálogo de filmes');
+  findAll() {
+    return this.listCatalogService.findAll();
   }
 
   @Get('film-catalog/:id')
-  findOne(@Param() params) {
-    return `Filme: ${params.id}`;
+  findOne(@Param('id') id) {
+    return this.listCatalogService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() body) {
-    return body;
+    return this.listCatalogService.create(body);
   }
 
   @Patch('film-catalog/:id')
-  update(@Param() params, @Body() body) {
-    return `
-    Atualização do Filme ${params.id}
-    `;
+  update(@Param() id: string, @Body() body) {
+    return this.listCatalogService.update(id, body);
   }
 
   @Delete('film-catalog/:id')
-  remove(@Param() params) {
-    return `
-    Deletar o Filme ${params.id}
-    `;
+  remove(@Param() id) {
+    return this.listCatalogService.remove(id);
   }
 }
